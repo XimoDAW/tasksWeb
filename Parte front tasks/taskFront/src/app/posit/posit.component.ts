61819c29783ea93039fe811e06a5edb7a39360e7
ximo <ximosanchezdelgado@gmail.com>
2024-03-23 18:10:56 +0100

    Mergeada parte back e inicializacion parte front

---
+++ Parte front tasks/taskFront/src/app/posit/posit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posit',
+  templateUrl: './posit.component.html',
+  styleUrls: ['./posit.component.css']
+})
+export class PositComponent {
+
+}
